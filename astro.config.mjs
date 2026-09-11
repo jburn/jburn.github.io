425f3f2619cc57d30e0c7dff13dec2ddde7c425c
Juho B <jopbruun@gmail.com>
2026-09-11 20:36:34 +0300

    work on base

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://jburn.github.io',
-  // Static files in assets/ are served from the site root.
-  publicDir: './assets',
   output: 'static',
   trailingSlash: 'always',
 });
