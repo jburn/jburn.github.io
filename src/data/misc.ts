fef70de012769ef8cc8b6aed94cfcf26a7f9b514
Juho B <jopbruun@gmail.com>
2026-09-25 18:16:10 +0300

    add misc tab

---
+++ src/data/misc.ts
@@ -0,0 +1,19 @@
+interface MiscCreation {
+  title: string;
+  summary: string;
+  image?: string;
+  alt?: string;
+  href?: string;
+  spotifyPlaylistId?: string;
+}
+
+
+export const miscCreations: MiscCreation[] = [
+  {
+    title: 'LINJA69',
+    summary: 'A list of the best eurodance and party music gathered during my time as a Wesibussi DJ that I like to keep up to date.',
+    spotifyPlaylistId: '6oKajZk2WkMlXCbFdEHaNH',
+    image: '/assets/misc/placeholder.svg',
+    alt: '',
+  },
+];
